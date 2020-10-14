36f2f98d5c235d5619f375ff53e4b5788fe99b27
khk7759 <48235716+khk7759@users.noreply.github.com>
2020-10-15 01:11:42 +0900

    etc

--- src/Level1/Exam16.java
+++ src/Level1/Exam16.java
@@ -1,9 +1,8 @@
 /* 
 	2020.10.15
-	written by hongkyoung Kim
-	
-	Exam16) 문자열 내 p와 y의 개수
 
+	Exam16) 문자열 내 p와 y의 개수
+	
 	대문자와 소문자가 섞여있는 문자열 s가 주어집니다. 
 	s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 
 	다르면 False를 return 하는 solution를 완성하세요. 
